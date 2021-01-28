74a58deb0f3f8eac2dc69ab8cdc05987dd8db82d
CictecMacMini <laoyouhub@icloud.com>
2021-01-28 15:11:10 +0800

    为线性查找添加范型支持

--- leetcode/src/com/leetcode/Linearsearch.java
+++ leetcode/src/com/leetcode/Linearsearch.java
@@ -1,14 +1,17 @@
 package com.leetcode;
 
 // 线性查找
+// 范型
+
 public class Linearsearch {
 
-    private Linearsearch(){}
+    private Linearsearch() {
+    }
 
-    static int search(int[] data, int target) {
+    static <E> int search(E[] data, E target) {
 
-        for (int i = 0 ; i < data.length; i++) {
-            if (data[i] == target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(target)) {
                 return i;
             }
         }
@@ -17,14 +20,11 @@ public class Linearsearch {
 
     public static void main(String[] args) {
 
-        int[] data = {2,3,4,5,96,6,7,8,8,85,9,000,0};
-
-        int index = Linearsearch.search(data,9);
+        Integer[] data = {2, 3, 4, 5, 96, 6, 7, 8, 8, 85, 9, 000, 0};
+        int index = Linearsearch.search(data, 9);
         System.out.println(index);
 
         int res = Linearsearch.search(data, 100);
         System.out.println(res);
-
-
     }
 }
